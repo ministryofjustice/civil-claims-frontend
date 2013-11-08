f43ef8f48b91067cf2f24065a455a7dcf91ddc65
Jairo Diaz <jairo.diaz@codescrum.com>
2013-11-08 17:52:59 +0000

    Added html ui for step 1 of claimant flow - personal details

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -3,4 +3,9 @@ class TemplatesController < ApplicationController
     @page_title = 'The user design template page'
     render 'templates/templates', :layout => 'application-claims'
   end
+
+  def step1
+    @page_title = 'Step 1 - Personal Details'
+    render 'templates/demo_step1', :layout => 'application-claims'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CivilClaimsFrontend::Application.routes.draw do
   get '/templates' => 'templates#templates'
+  get '/step1' => 'templates#step1'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
