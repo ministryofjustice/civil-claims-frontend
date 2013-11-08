1ee42bcb40f36bb23d74bfab582af94ac91c3ff0
Jairo Diaz <jairo.diaz@codescrum.com>
2013-11-08 15:41:53 +0000

    Added title to the templates page so header is visible on the template

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,5 +1,6 @@
 class TemplatesController < ApplicationController
   def templates
+    @page_title = 'The user design template page'
     render 'templates/templates', :layout => 'application-claims'
   end
 end
