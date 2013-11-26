b2872c48bcb200c151c08d68dd9a94e3dac81835
Jairo Diaz <jairo.diaz@codescrum.com>
2013-11-26 11:56:57 +0000

    Added test on claim for lanlord attribute

--- spec/models/claim_spec.rb
+++ spec/models/claim_spec.rb
@@ -15,6 +15,21 @@ describe Claim do
         "land_registry_title_number" => "26736736",
         "repossession_claim_id" => nil
       },
+      :landlord => {
+        "street" => "103",
+        "town" => "Bristol",
+        "postcode" => "N2 2QX",
+        "id" => 87,
+        "repossession_claim_id" => nil,
+        "company_name" => 'Bristol County Council',
+        "title" => 'Mr.',
+        "full_name" => 'Jairo Pereira',
+        "phone" => '07 254 121 232',
+        "mobile" => '932 29 22',
+        "email" => 'bristol@gov.uk',
+        "dx_number" => 'dx 23626',
+        "dx_exchange" => 'ex 282'
+      },
       :tenants => [
         {
           "title" => "Miss",
@@ -33,6 +48,7 @@ describe Claim do
 
   it { should respond_to :id }
   it { should respond_to :property }
+  it { should respond_to :landlord }
   it { should respond_to :tenants }
 
   # It does not seem to work when initalized with json
