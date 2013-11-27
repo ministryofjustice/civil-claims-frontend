4f67949a4ac19c5c2763fa5d7eda755a30d42493
Jairo Diaz <jairo.diaz@codescrum.com>
2013-11-27 17:44:06 +0000

    Configured default root to point to the first step

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ CivilClaimsFrontend::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'claims#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
