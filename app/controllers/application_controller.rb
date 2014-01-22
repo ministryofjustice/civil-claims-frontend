480adea0cf79fae60cc09b8a32554278709f3717
Rob McKinnon <@digital.justice.gov.uk>
2014-01-22 11:28:55 +0000

    set_secure_token around filter now set via Author mixin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,14 @@
-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_signed_in
-  around_action :set_secure_token
-
+  include Author::Controller
+  
   def flash_alert(message)
     flash[:alert] ||= []
     flash[:alert] << message
   end
-
-  include Author::Controller
 
   def api_models
     [Claim, User]
@@ -23,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_to_login_page_if_not_signed_in
-    unless @signed_in
+    unless signed_in?
       redirect_to controller: 'users', action: 'login_screen'
     end
   end
